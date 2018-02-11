911e137aa2b799f87fe3f4bae9552ac184c28013
Oleg Dudkin <dudkin.on@gmail.com>
2018-02-11 00:30:52 -0600

    feat: Add ScrollMagic lib

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './fa/css/fontawesome-all.min.css';
 import './App.css';
-
+import * as ScrollMagic from "scrollmagic";
 
 class App extends Component {
 
@@ -11,6 +11,9 @@ class App extends Component {
   render() {
 
     let scope = this;
+    let controller = new ScrollMagic.Controller();
+
+
 
     return (
       <div>
