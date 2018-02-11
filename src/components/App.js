4f062a4515ca0768539fa676afb97dc41964e574
Oleg Dudkin <dudkin.on@gmail.com>
2018-02-11 00:27:37 -0600

    feat: Create app

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './fa/css/fontawesome-all.min.css';
+import './App.css';
+
+
+class App extends Component {
+
+
+  render() {
+
+    let scope = this;
+
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
