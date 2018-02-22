0cc45c21c8675f47924440fea8a4d220ec25af81
Oleg Dudkin <dudkin.on@gmail.com>
2018-02-22 04:16:39 -0600

    feat: Add redux libs to project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools as compose } from 'redux-devtools-extension';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+
+const store = createStore(reducer, compose(applyMiddleware(thunk)));
+const history = syncHistoryWithStore(browserHistory, store);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
+
+
