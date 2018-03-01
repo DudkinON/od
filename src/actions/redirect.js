398451508b6c419131ad94c725f6f14568d124fe
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-01 16:38:30 -0600

    fix: Hide email

--- src/actions/redirect.js
+++ src/actions/redirect.js
@@ -10,7 +10,7 @@ export const redirect = (url, height = 700, width = 1000) => {
   return () => {
     const strWindowFeatures = "location=yes,height="+height+",width="+width+",scrollbars=yes,status=yes";
     let URL;
-    if (url === 'mail@oleg-dudkin.com') URL = "mailto:" + url + "?Subject=The%20subject";
+    if (url === 'mail'+'@'+'oleg-dudkin'+'.com') URL = "mailto:" + url + "?Subject=The%20subject";
     else URL = url;
     window.open(URL, "_blank", strWindowFeatures);
   };
