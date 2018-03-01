4feb9afe43e9c694a38a53999d66da7f4429f8a8
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-01 16:47:30 -0600

    feat: Remove excess code from Certificates component

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,77 +1,38 @@
-import React from 'react'
-import CertificateItem from './CertificateItem'
-import $ from "jquery";
+import React from 'react';
+import CertificateBlock from './CertificateBlock';
+import {connect} from "react-redux";
+import {getCertificate} from "../actions/getCertificate";
 
 
 class Certificates extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      right: "-100%",
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-
-  componentDidMount() {
-    fetch(this.props.url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
 
   render() {
 
-    const { error, isLoaded, items } = this.state;
+    this.props.onGetCertificate(this.props.certificates.url);
 
-    $(document).ready(function(){
-      $('#certificate').viewportChecker({
-        classToRemove: 'invisible',
-        classToAdd: 'visible fadeInRight',
-        offset: 300});
-    });
 
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-
-      return (
-        <section className="certificates animated invisible" id="certificate">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-2 mr-4">
-                <h3 className="certificates-title">Certificates</h3>
-                <div className="icon text-center"><i className="fas fa-certificate" aria-hidden="true" /></div>
-              </div>
-              <div className="col-lg-10 row">
-                {items.map( (item, i) =>
-                  <CertificateItem item={item} key={"cert_" + i} i={i} />
-                )}
-              </div>
+    return (
+      <section className="certificates animated invisible" id="certificate">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-2 mr-4">
+              <h3 className="certificates-title">{this.props.certificates.title}</h3>
+              <div className="icon text-center"><i className="fas fa-certificate" aria-hidden="true" /></div>
             </div>
+            <CertificateBlock />
           </div>
-        </section>
-      );
-    }
+        </div>
+      </section>
+    );
   }
 }
 
-export default Certificates;
+export default connect(
+  state => ({certificates: state.provider.certificates}),
+  dispatch => ({
+    onGetCertificate: (url) => {
+      dispatch(getCertificate(url));
+    }
+  })
+)(Certificates);
