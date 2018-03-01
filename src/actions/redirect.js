d6355128728fa41a532984d4fc8ebea65cacda85
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-01 16:36:02 -0600

    docs: Add JS doc to redirect function

--- src/actions/redirect.js
+++ src/actions/redirect.js
@@ -1,5 +1,12 @@
-
 export const redirect = (url, height = 700, width = 1000) => {
+  /**
+   * Open 'url' in a new window  with 'height' (default 700) and 'width' (default 1000)
+   *
+   * @param url: string
+   * @param height: int
+   * @param width: int
+   * @return function
+   */
   return () => {
     const strWindowFeatures = "location=yes,height="+height+",width="+width+",scrollbars=yes,status=yes";
     let URL;
