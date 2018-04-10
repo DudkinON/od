db02d6db24f7cb3b5b19a2e488b5322bbf58a546
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-10 06:36:26 -0500

    feat: Remove control data from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,41 @@
 import React, {Component} from 'react';
-import Provider from './Provider';
 import Header from './Header';
 import About from './About';
 import Skills from './Skills';
 import Experience from './Experience';
 import Education from './Education';
 import Certificates from './Certificates';
-import Works from './Works'
-import Contacts from './Contacts'
-import Footer from './Footer'
+import Works from './Works';
+import Contacts from './Contacts';
+import Footer from './Footer';
+import { connect } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './fa/css/fontawesome-all.min.css';
 import 'animate.css'
 import './App.css';
-import * as viewportChecker from './jq.vp';
-
+import './jq.vp';
 
 class App extends Component {
 
   render() {
 
-    let scope = this;
-    scope.data = Provider;
-    scope.viewportChecker = viewportChecker;
-
-
     return (
       <div>
-        <Header main={scope.data.header} scope={scope}/>
-        <About main={scope.data.about} scope={scope}/>
-        <Skills main={scope.data.skills} scope={scope}/>
-        <Experience main={scope.data.experience} scope={scope}/>
-        <Education main={scope.data.education} scope={scope}/>
-        <Certificates url={scope.data.certificates} scope={scope}/>
-        <Works url={scope.data.works} scope={scope}/>
-        <Contacts social={scope.data.social} scope={scope}/>
-        <Footer scope={scope}/>
+        <Header />
+        <About />
+        <Skills />
+        <Experience />
+        <Education />
+        <Certificates />
+        <Works />
+        <Contacts />
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  state => ({provider: state.provider}),
+  dispatch => ({})
+)(App);
