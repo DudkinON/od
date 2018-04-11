e493007f018c652288f8a9cfd67e0e20a04c4225
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-11 10:06:02 -0500

    feat: Create WorkItem component

---
+++ src/components/WorksItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+class WorksItem extends React.Component {
+
+
+  render() {
+
+    const item = this.props.item;
+
+
+    return (
+        <div className="col-sm-12 col-md-6 text-center work-item animated invisible">
+          <div className="card-item"
+               style={{backgroundImage: item.img}}>
+            <img src={item.images} alt={item.title} />
+            <div className="card-container animated" style={{opacity: context}}>
+              <h6>{item.title}</h6>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        </div>
+    );
+  }
+}
+
+export default WorksItem;
