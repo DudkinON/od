18b1bea4a852edc4803bb1837a1aae03ca1b0082
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-11 09:52:51 -0500

    feat: Add Redux to Works component

--- src/components/Works.js
+++ src/components/Works.js
@@ -1,75 +1,39 @@
 import React, {Component} from 'react'
 import WorkElement from "./WorkElement";
-import $ from "jquery";
+import {connect} from "react-redux";
+import {getWorks} from "../actions/getWorks";
 
 
 class Works extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      items: []
-    };
-  }
-
-
-  componentDidMount() {
-    fetch(this.props.url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            items: result
-          });
-        },
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        }
-      )
-  }
-
 
   render() {
 
-    const {error, isLoaded, items} = this.state;
+    this.props.onGetWorks(this.props.base + this.props.works.url);
 
-    $(document).ready(function(){
-      $('#works').viewportChecker({
-        classToRemove: 'invisible',
-        classToAdd: 'visible fadeInLeft',
-        offset: 300});
-    });
 
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <section className="works animated invisible" id="works">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-2">
-                <h3 className="skills-title">Works</h3>
-                <div className="white-icon"><i className="fas fa-file-code"/></div>
-              </div>
-              <div className="col-lg-10 row">
-                {items.map( (item, i) =>
-                  <WorkElement item={item} key={"work_" + i} i={i}/>
-                )}
-              </div>
+    return (
+      <section className="works animated invisible" id="works">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-2">
+              <h3 className="skills-title">{this.props.works.title}</h3>
+              <div className="white-icon"><i className="fas fa-file-code"/></div>
             </div>
+            <WorkElement />
           </div>
-        </section>
-      );
-    }
+        </div>
+      </section>
+    );
+
   }
 }
 
-export default Works;
+export default connect(
+  state => ({works: state.provider.works, base: state.provider.base}),
+  dispatch => ({
+    onGetWorks: (url) => {
+      dispatch(getWorks(url));
+    }
+  })
+)(Works);
