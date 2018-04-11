9702ea4709be1c9c320b4e954db325ac65c8ad3d
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-11 09:07:48 -0500

    feat: Add Rudux to About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,34 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {connect} from "react-redux";
 
 
 class About extends Component {
-    render() {
+  render() {
 
-        const data = this.props.main;
+    const data = this.props.about;
 
-        return (
-            <section className="about appear-animated" id="about">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-3">
-                            <h3 className="about-title">about</h3>
-                            <div className="icon">
-                                <i className="fas fa-user" />
-                            </div>
-                        </div>
-                        <div className="col-lg-9">
-                            <h2 className="about-name">{data.nama}</h2>
-                            <div className="about-description">{data.description}</div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        );
-    }
+    return (
+      <section className="about appear-animated" id="about">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3">
+              <h3 className="about-title">{this.props.title}</h3>
+              <div className="icon">
+                <i className="fas fa-user"/>
+              </div>
+            </div>
+            <div className="col-lg-9">
+              <h2 className="about-name">{data.name}</h2>
+              <div className="about-description">{data.description}</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
 
-export default About;
+export default connect(
+  state => ({about: state.info, title: state.provider.about.title}),
+  dispatch => ({})
+)(About);
