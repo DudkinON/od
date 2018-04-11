cf65d67d0a460ee8097fdceae479280b439194aa
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-11 10:07:29 -0500

    feat: Add redirect to WorkItem component

--- src/components/WorksItem.js
+++ src/components/WorksItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {redirect} from "../actions/redirect";
 
 
 class WorksItem extends React.Component {
@@ -8,11 +9,11 @@ class WorksItem extends React.Component {
 
     const item = this.props.item;
 
-
     return (
         <div className="col-sm-12 col-md-6 text-center work-item animated invisible">
           <div className="card-item"
-               style={{backgroundImage: item.img}}>
+               style={{backgroundImage: item.img}}
+               onClick={redirect(item.url)}>
             <img src={item.images} alt={item.title} />
             <div className="card-container animated" style={{opacity: context}}>
               <h6>{item.title}</h6>
