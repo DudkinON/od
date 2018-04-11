aae3eb1bbf52060c549462839a74e1fe61082b95
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-11 09:50:07 -0500

    feat: Migrate data from Provider to database

--- src/components/Provider.js
+++ src/components/Provider.js
@@ -1,187 +1,36 @@
 const Provider = {
+  base: 'http://0.0.0.0:5000',
   header: {
-    title: "full stack web developer",
-    slogan: "The right decision for your project",
-    description: "Create everything from small web projects to large interactive web applications."
+    url: "/info"
   },
   about: {
-    name: "Oleg Dudkin",
     title: "about",
-    description:
-    'Offering creative design solutions and fast working code to solve business problems is what I\n' +
-    'do best. My projects are primarily written in Python and PHP programming languages, while\n' +
-    'Javascript and CSS are used for design. I also have extensive experience with SQL databases as\n' +
-    'well as building administration servers. I enjoy working on scalability, performance, responsive\n' +
-    'design, and focus on creating a great user experience.\n' +
-    'As a web designer and developer, writing clear code that conforms to industry standards and\n' +
-    'adheres to the best semantic practices is of the utmost importance and is always reflected in\n' +
-    'my work.'
+    url: "/info"
   },
   skills: {
     title: "skills",
-    list: [
-      {
-        title: "Programming languages",
-        list: [
-          {
-            title: "PHP",
-            percent: 78
-          },
-          {
-            title: "Python",
-            percent: 71
-          },
-          {
-            title: "JavaScript",
-            percent: 66
-          },
-          {
-            title: "Java",
-            percent: 30
-          },
-          {
-            title: "C++",
-            percent: 10
-          }
-        ]
-      },
-      {
-        title: "Design",
-        list: [
-          {
-            title: "ANGULAR JS",
-            percent: 81
-          },
-          {
-            title: "React JS",
-            percent: 76
-          },
-          {
-            title: "BOOTSTRAP",
-            percent: 75
-          },
-          {
-            title: "jQuery",
-            percent: 70
-          },
-          {
-            title: "KnockoutJS",
-            percent: 40
-          }
-        ]
-      },
-      {
-        title: "databases",
-        list: [
-          {
-            title: "PostgreSQL",
-            percent: 82
-          },
-          {
-            title: "MySQL",
-            percent: 79
-          },
-          {
-            title: "SQLite",
-            percent: 78
-          },
-          {
-            title: "MongoDB",
-            percent: 25
-          }
-        ]
-      },
-      {
-        title: "administration",
-        list: [
-          {
-            title: "NGINX web server",
-            percent: 79
-          },
-          {
-            title: "Apache HTTP Server",
-            percent: 68
-          },
-          {
-            title: "Linux OS",
-            percent: 65
-          },
-          {
-            title: "Linux Security",
-            percent: 60
-          },
-
-        ]
-      }
-    ]
+    url: "/skills"
   },
   experience: {
     title: "Experience",
-    list: [
-      {
-        title: "Freelance",
-        description: "Front-End Web development",
-        start: 2009,
-        end: 2012
-      },
-      {
-        title: "Freelance",
-        description: "PHP web developer",
-        start: 2012,
-        end: 2014
-      },
-      {
-        title: "OOO ROSSA",
-        description: "PHP web developer",
-        start: 2014,
-        end: 2015
-      },
-      {
-        title: "SK MIAL",
-        description: "Back end developer",
-        start: 2015,
-        end: 2016
-      },
-    ]
+    url: "/experience"
   },
   education: {
-    title: "Education",
-    name: "Saint-Petersburg University of Management and Economics Technologies",
-    description: "Under the scientific and methodological guidance of the OON RAS",
-    specialisation: "Specialisation: Applied Informatics in Analytical Economics",
-    url: "http://www.spbume.ru/",
-    start: 2015,
-    end: 2019
+    title: "education",
+    url: "/education"
   },
-  certificates: "/certificates.json",
-  works: "/works.json",
-  social: [
-    {
-      title: "E-mail",
-      url: "mail@oleg-dudkin.com",
-      style: "fa fa-envelope"
-    },
-    {
-      title: "Linkedin",
-      url: "https://www.linkedin.com/in/oleg-dudkin/",
-      style: "fab fa-linkedin"
-    },
-    {
-      title: "GitHub",
-      url: "https://github.com/DudkinON",
-      style: "fab fa-github"
-    },
-    {
-      title: "Facebook",
-      url: "https://www.facebook.com/oleg.dudkin.50",
-      style: "fab fa-facebook"
-    },
-    {
-      title: "Twitter",
-      url: "https://twitter.com/DudkinOleg",
-      style: "fab fa-twitter"
-    },
-  ]
+  certificates: {
+    title: "Certificates",
+    url:"/certificates"
+  },
+  works: {
+    title: "works",
+    url: "/works"
+  },
+  social: {
+    url: "/social"
+  },
+  website: "oleg-dudkin.com"
 };
 
 
