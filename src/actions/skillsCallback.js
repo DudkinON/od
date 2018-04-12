90ebd286d907a44e3a72822d0646ac7459ccbad5
Oleg Dudkin <dudkin.on@gmail.com>
2018-04-12 08:27:10 -0500

    docs: Add docstring to skillsCallback function

--- src/actions/skillsCallback.js
+++ src/actions/skillsCallback.js
@@ -1,6 +1,8 @@
 export const skillsCallback = () => {
-
-
+  /**
+   * In the loop run each function
+   * @return void:
+   */
   let elements = document.getElementsByClassName('progress-bar');
   [].forEach.call(elements, function(e) {
     const percent = e.getAttribute('aria-valuenow');
