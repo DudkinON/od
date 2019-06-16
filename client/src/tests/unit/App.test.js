bbd36580de04c454ea5a10fc3478eb1413835121
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-16 13:17:33 -0700

    feat: Add tests for App component

---
+++ client/src/tests/unit/App.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {shallow} from "enzyme";
+// import toJson from 'enzyme-to-json';
+import App from "../../App";
+
+
+describe("Tests for App", () => {
+  const app = shallow(<App />);
+
+  it('should render component correctly', function () {
+    expect(app).toMatchSnapshot();
+  });
+
+  it("checks that App contains Header component", function () {
+    expect(app.find('Header').length).toBe(1);
+  });
+
+  it('checks that App contains Footer component', function () {
+    expect(app.find('Footer').exists()).toBe(true);
+  });
+
+});
