5b0c61a5ef0f96c1a3ed0b38555ff245fa81249a
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-20 19:53:09 -0700

    refactor: Use Switch component in the router

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Landing from './components/Landing';
 import Footer from './components/Footer';
 
@@ -10,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Route exact to="/" component={Landing}/>
+      <Switch>
+        <Route exact path="/" component={Landing}/>
+      </Switch>
       <Footer />
     </BrowserRouter>
   );
