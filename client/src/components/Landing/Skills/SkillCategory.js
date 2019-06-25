9c40da400cefb71e406d26c986553c791f654107
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-24 18:58:26 -0700

    feat: Add SkillCategory component

---
+++ client/src/components/Landing/Skills/SkillCategory.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import Chart from "./Chart";
+
+
+class SkillCategory extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      skills: []
+    }
+  }
+
+  showSkills = () => {
+    if (this.state.skills.length > 0) this.setState({skills: []});
+    else this.setState({skills: this.props.content});
+  };
+
+  render() {
+    return (
+      <div className="skills__category-container">
+        <h3 onClick={this.showSkills} className="skills__category">{this.props.header}</h3>
+        <div className="skills__body">
+          {this.state.skills.map(props => (
+            <div className="skills__block" key={props.name}>
+              <Chart {...props}/>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SkillCategory;
