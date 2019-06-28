ac06c149b80c7ee5f587037db9f222da0322252f
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-28 12:14:02 -0700

    feat: Add tests for CertificateBlock comonent

--- client/src/components/Landing/Certificates/CertificateBlock.test.js
+++ client/src/components/Landing/Certificates/CertificateBlock.test.js
@@ -10,11 +10,44 @@ describe('CertificateBlock', () => {
     "title": "certificate one",
     "description": "description of the certificate",
     "date": "14/15/2018",
-    "img": "/path/to/image/certificate"
+    "img": "/path/to/image/certificate",
+    "button": "button name"
   };
+
+  global.open = jest.fn();
+
   const certificateBlock = shallow(<CertificateBlock {...props}/>);
 
   it('should render properly', function () {
     expect(certificateBlock).toMatchSnapshot();
+  });
+
+  it('should display passed title', function () {
+    expect(certificateBlock.find('.certificates__view-header').text())
+      .toBe(props.title);
+  });
+
+  it('should display passed description', function () {
+    expect(certificateBlock.find('.certificates__view-description').text())
+      .toBe(props.description)
+  });
+
+  it('should display button name', function () {
+    expect(certificateBlock.find('.certificates__view-link').text())
+      .toBe(props.button);
+  });
+
+  it('should display passed image', function () {
+
+    // contains passed image url
+    expect(certificateBlock.find('img').html()).toContain(props.img);
+
+    // contains passed title as alt attribute
+    expect(certificateBlock.find('img').html()).toContain(props.title);
+  });
+
+  it('should call url if user click the button', function () {
+    certificateBlock.find('.certificates__view-link').simulate('click');
+    expect(global.open).toHaveBeenCalled();
   });
 });
