90e943db16fc4262828381c8c94999f6e62d1ef3
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-28 12:54:09 -0700

    feat: Add works reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import config from './config';
 import skills from './skills';
 import certificates from './certificates';
+import works from './works';
 
 
 export default combineReducers({
   config,
   skills,
-  certificates
+  certificates,
+  works
 });
