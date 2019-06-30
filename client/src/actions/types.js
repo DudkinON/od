5e2e4c77d92ee739b74a9d018759a2844abc47c2
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-30 10:16:06 -0700

    feat: Add `contact` reducer

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,3 +1,4 @@
 export const SET_SKILLS = 'SET_SKILLS';
 export const SET_CERTIFICATES = 'SET_CERTIFICATES';
 export const SET_WORKS = 'SET_WORKS';
+export const SET_CONTACT = 'SET_CONTACT';
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,11 +3,13 @@ import config from './config';
 import skills from './skills';
 import certificates from './certificates';
 import works from './works';
+import contact from './contact';
 
 
 export default combineReducers({
   config,
   skills,
   certificates,
-  works
+  works,
+  contact
 });
