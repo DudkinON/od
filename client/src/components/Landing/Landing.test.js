e2d4079d688da8026adcb420beda1e72c0d3e259
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-30 22:19:00 -0700

    feat: Add tests for Lending component

--- client/src/components/Landing/Landing.test.js
+++ client/src/components/Landing/Landing.test.js
@@ -14,4 +14,20 @@ describe('Landing', () => {
   it('should contains `CenterBlock` component', function () {
     expect(landing.find('Connect(CenterBlock)').exists()).toBe(true);
   });
+
+  it('should contains Skills component', function () {
+    expect(landing.find('Connect(Skills)').exists()).toBe(true);
+  });
+
+  it('should contains Certificates component', function () {
+    expect(landing.find('Connect(Certificates)').exists()).toBe(true);
+  });
+
+  it('should contains Works component', function () {
+    expect(landing.find('Connect(Works)').exists()).toBe(true);
+  });
+
+  it('should contains Contact component', function () {
+    expect(landing.find('Connect(Contact)').exists()).toBe(true);
+  });
 });
