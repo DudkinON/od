f5e74ce565bacbbe8daa67eb14035e23593d35a5
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-02 15:16:10 -0700

    refactor: Update header styles

--- client/src/components/Header/Nav.js
+++ client/src/components/Header/Nav.js
@@ -17,26 +17,29 @@ export class Nav extends Component {
     const {links} = this.props;
 
     return (
-      <nav className="header__nav_top" >
+      <nav className="header__nav" >
         <input type="checkbox"
                className="header__nav_checkbox"
                id="header__nav_toggle"/>
-        <label htmlFor="header__nav_toggle" className="header__nav_button">
-          <span className="header__nav_icon"/>
-        </label>
-        <Link to="/">
-          <img src="/img/logo.svg" className="header__nav_logo" alt=""/>
-        </Link>
-        <ul className='header__nav_menu'>
-          <li className="push"/>
-          {links.map(link => (
-            <li key={link.name}>
-              <Link className="header__nav_link" to={link.url} >
-                <span data-hover={link.name}>{link.name}</span>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <div className="header__background" />
+        <div className="header__content">
+          <label htmlFor="header__nav_toggle" className="header__nav_button">
+            <span className="header__nav_icon"/>
+          </label>
+          <Link to="/" className="header__logo-container">
+            <img src="/img/logo.svg" className="header__nav_logo" alt=""/>
+          </Link>
+          <ul className='header__nav_menu'>
+            <li className="push"/>
+            {links.map(link => (
+              <li key={link.name}>
+                <Link className="header__nav_link" to={link.url} >
+                  <span data-hover={link.name}>{link.name}</span>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
       </nav>
     );
   }
