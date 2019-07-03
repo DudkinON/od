67a5fc00a8fd611ba3f1f5e6f0582e5b9f63658f
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-02 17:21:11 -0700

    refactor: Update styles of Skills section

--- client/src/components/Landing/Skills/SkillCategory.js
+++ client/src/components/Landing/Skills/SkillCategory.js
@@ -8,22 +8,32 @@ class SkillCategory extends Component {
     super(props);
 
     this.state = {
-      skills: []
+      skills: [],
+      classes: "skills__body"
     }
   }
 
   showSkills = () => {
-    if (this.state.skills.length > 0) this.setState({skills: []});
-    else this.setState({skills: this.props.content});
+    if (this.state.skills.length > 0) {
+      const classes = "skills__body";
+      this.setState({skills: [], classes});
+    } else {
+      const classes = "skills__body skills__active";
+      this.setState({skills: this.props.content, classes});
+    }
   };
 
   render() {
     return (
       <div className="skills__category-container">
-        <h3 onClick={this.showSkills} className="skills__category">{this.props.header}</h3>
-        <div className="skills__body">
+        <h3 onClick={this.showSkills} className="skills__category">
+          <span className="skills__category_btn">
+          {this.props.header}
+          </span>
+        </h3>
+        <div className={this.state.classes}>
           {this.state.skills.map(props => (
-            <div className="skills__block" key={props.name}>
+            <div className="skills__block" key={props.id}>
               <Chart {...props}/>
             </div>
           ))}
