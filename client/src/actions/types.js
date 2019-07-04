720e11bb13884d282ebd040c9b7139dc8714536f
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-04 16:51:27 -0700

    feat: Update actions

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,4 +2,5 @@ export const SET_SKILLS = 'SET_SKILLS';
 export const SET_CERTIFICATES = 'SET_CERTIFICATES';
 export const SET_WORKS = 'SET_WORKS';
 export const SET_CONTACT = 'SET_CONTACT';
+export const SET_BENEFITS = 'SET_BENEFITS';
 
