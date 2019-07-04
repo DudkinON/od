c4f874e49bc0aae4660d1bf8e0079262b653fcab
Oleg Dudkin <dudkin.on@gmail.com>
2019-07-04 16:48:56 -0700

    feat: Add tests for reducers

---
+++ client/src/reducers/reducers.test.js
@@ -0,0 +1,31 @@
+import reducers from './index';
+
+
+describe('reducers', () => {
+
+  const list = reducers(undefined, {});
+
+  it('should contain config reducer', function () {
+    expect(list).toHaveProperty('config');
+  });
+
+  it('should contain skills reducer', function () {
+    expect(list).toHaveProperty('skills');
+  });
+
+  it('should contain certificates reducer', function () {
+    expect(list).toHaveProperty('certificates');
+  });
+
+  it('should contain works reducer', function () {
+    expect(list).toHaveProperty('works');
+  });
+
+  it('should contain contact reducer', function () {
+    expect(list).toHaveProperty('contact');
+  });
+
+  it('should contain benefits reducer', function () {
+    expect(list).toHaveProperty('benefits');
+  });
+});
